416e00a852f6f388ff3ae7e3130a7dc64a6c43e0
David E. Perez Negron R. <david@nethunters.xyz>
2024-04-09 20:36:54 -0600

    base config

---
+++ _config.ts
@@ -0,0 +1,12 @@
+import lume from "lume/mod.ts";
+import relativeUrls from "lume/plugins/relative_urls.ts";
+import blog from "blog/mod.ts";
+
+const site = lume({
+  location: new URL("https://blog.deca.eco"),
+});
+
+site.use(relativeUrls());
+site.use(blog());
+
+export default site;
