70db29d578be9974283f3ec9bc167838644efd48
David E. Perez Negron R. <david@nethunters.xyz>
2024-05-01 17:15:35 -0600

    prepare dev site for testing

--- _config.ts
+++ _config.ts
@@ -3,7 +3,8 @@ import relativeUrls from "lume/plugins/relative_urls.ts";
 import blog from "blog/mod.ts";
 
 const site = lume({
-  location: new URL("https://blog.deca.eco"),
+  location: new URL("https://dev.deca.eco"),
+  //location: new URL("https://deca.eco"),
 });
 
 site.copy("img");
